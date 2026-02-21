11e76820c103deb5e1d5cd8ae77842224df2f348
miatran27 <tranm622@newschool.edu>
2026-02-21 16:32:22 -0500

    added a function in for the "back to top" button as user scrolls through the channel blocks

--- script.js
+++ script.js
@@ -57,3 +57,26 @@ function toggleQuote(btn) {
 // I want to add a "read more" button to expand the text and "read less" button once text is expanded fully to close the text
 // I pulled this code from Claude.ai
 // Here the "extra.style.display" is my extra 'block' that I put in my span class "quote-more" in HTML, that block will be hidden with a "Read more" button displayed for users to expand the block. Otherise, when that 'block' is fully displayed, there will be a "Read less" button to collapse the block.
+
+
+// I want to add "back to top" button as user scrolls through the channel blocks
+// I got this idea from a friend and Claude.ai 
+// Here I'm defining what each element is and giving it a function to each action
+
+let backToTop = document.querySelector('.back-to-top')
+let channelSection = document.querySelector('.channel-blocks-section')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= channelSection.offsetTop) {
+        backToTop.classList.add('visible')
+    } 
+
+    //the line above means if the user has scrolled down far enough to reach where the channel blocks section starts, add the 'visible' class, which triggers the CSS to show the button.
+    //"backToTop.classList" means calling the class list that belongs to the "back-to-top" button
+    
+    else {
+        backToTop.classList.remove('visible')
+    }
+
+    //this means otherwise if the condition is not applied (when user is not scrolling through the channel blocks section, so when user scrolls back up and it reaches the area above the channel blocks section) → remove 'visible' (hide the button)
+})
