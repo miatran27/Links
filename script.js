0aeeadcbdff64d3a07536ed0c87c166fd2de191f
miatran27 <tranm622@newschool.edu>
2026-02-21 14:34:11 -0500

    made the description section collapsable by adding "read more" and "read less" button

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ showLinksButton.addEventListener('click', () => {
     channelBlocks.classList.add('show-links')
 })
 
+function toggleQuote(btn) {
+    let extra = btn.previousElementSibling
+    if (extra.style.display === 'block') {
+        extra.style.display = 'none'
+        btn.textContent = 'Read more'
+    } 
+    //'block' is hidden add there is a "Read more" button for users to click to expand and display the 'block'
+    else {
+        extra.style.display = 'block'
+        btn.textContent = 'Read less'
+    }
+    //when 'block' is fully displayed, there's a "Read less" button to collapse the 'block'
+}
+
+// I want to add a "read more" button to expand the text and "read less" button once text is expanded fully to close the text
+// I pulled this code from Claude.ai
+// Here the "extra.style.display" is my extra 'block' that I put in my span class "quote-more" in HTML, that block will be hidden with a "Read more" button displayed for users to expand the block. Otherise, when that 'block' is fully displayed, there will be a "Read less" button to collapse the block.
